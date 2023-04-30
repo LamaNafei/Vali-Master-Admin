49790dbfd39d2467a465ac206f39097db3bdc321
LamaNafei <lama.nafei@gmail.com>
2023-04-30 12:49:30 +0300

    searchBar done

--- controllers/user/user.js
+++ controllers/user/user.js
@@ -1,13 +1,13 @@
 const bcrypt = require('bcrypt');
 const validator = require('validator');
-const search = require('../../services/user')
+const model = require('../../services/user')
 
 async function logInController(res, req){
   if(req.method == "POST"){
     const sanitizedEmail = validator.escape(req.body.email);
     const sanitizedPassword = validator.escape(req.body.password);
     try {
-      const user = await search(sanitizedEmail);
+      const user = await model.search(sanitizedEmail);
       if (!user) {
           return res.json({"message": 'Email Not Found'})
       }
